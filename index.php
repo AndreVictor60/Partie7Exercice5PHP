d228bf617e304b4f3f0a9329185fe91376c79620
AndreVictor60 <andre.victor60@free.fr>
2018-01-03 10:58:32 +0100

    Commentaire

--- index.php
+++ index.php
@@ -1,24 +1,45 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>Partie 7 Exercice 5</title>
-  </head>
-  <body>
+    <head>
+        <meta charset="utf-8">
+        <title>Partie 7 Exercice 5</title>
+    </head>
+
+    <body>
+        <p>Créer un formulaire sur la page index.php avec :
+        <ol>
+            <li>Une liste déroulante pour la civilité (Mr ou Mme)</li>
+            <li>Un champ texte pour le nom</li>
+            <li>Un champ texte pour le prénom</li>
+        </ol>
+        Ce formulaire doit rediriger vers la page index.php.
+        Vous avez le choix de la méthode.</p>
+    <hr>
+    <!-- Création du formulaire
+ Action = L'attribut sert à définir la page appelée par le formulaire
+ Method(variable globale) = Méthode d'envoie du formulaire GET ou POST(99%)
+    -->
     <form action="index.php" method="post">
-      <select name="genre">
-          <option value="Monsieur">Mr</option>
-          <option value="Madame">Mme</option>
-      </select>
-      <label for="firstName">Nom :</label><input type="text" name="firstName">
-      <label for="lastName">Prénom :</label><input type="text" name="lastName">
-      <input type="submit" value="valider">
+        <!-- Créer une liste HTML avec les valeurs suivantes -->
+        <select name="genre">
+            <option value="Monsieur">Mr</option>
+            <option value="Madame">Mme</option>
+        </select>
+        <!-- Ajout des inputs avec leurs labels -->
+        <label for="firstName">Nom :</label><input type="text" name="firstName">
+        <label for="lastName">Prénom :</label><input type="text" name="lastName">
+        <!-- Submit : le bouton de validation du formulaire qui commande l'envoi des donnée-->
+        <input type="submit" value="Valider">
+        <a href="../index.php"><button type="button">Retour</button></a>
     </form>
+
     <?php
-      if (isset($_POST['genre']) AND isset($_POST['firstName']) AND isset($_POST['lastName']))
-      {
-        echo $_POST['genre'] . ', ' . $_POST['firstName']. ' ' .$_POST['lastName'];
-      }
-     ?>
-  </body>
+    // Vérification des POST s'ils existent
+    if (isset($_POST['genre']) && isset($_POST['firstName']) && isset($_POST['lastName'])) {
+        // htmlspecialchars qui sert à échapper le code HTML ( Éviter les failles XSS )
+        echo htmlspecialchars($_POST['genre']) . ', ' . htmlspecialchars($_POST['firstName']) . ' ' . htmlspecialchars($_POST['lastName']);
+    }
+    ?>
+
+</body>
 </html>
